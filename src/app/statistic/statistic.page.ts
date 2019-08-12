4761d341b003d850b20c3d69f43e139993b564f4
sumragen <Sumragen@gmail.com>
2019-08-12 22:05:31 +0300

    chart calculation

--- src/app/statistic/statistic.page.ts
+++ src/app/statistic/statistic.page.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from "@angular/core";
+import {StorageService} from "../shared/services/storage/storage.service";
 
 @Component({
     selector: 'app-statistic',
@@ -7,22 +8,39 @@ import {Component, OnInit} from "@angular/core";
 })
 export class StatisticPage implements OnInit {
 
-    public doughnutChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
-    public doughnutChartData: number[] = [350, 450, 100];
+    public doughnutChartLabels: string[] = [];
+    public doughnutChartData: number[] = [];
     public doughnutChartType: string = 'doughnut';
 
-    constructor() {
+    constructor(private storageService: StorageService) {
     }
 
     ngOnInit() {
+        this.calculateStorageData();
     }
 
-// events
     public chartClicked(e: any): void {
         console.log(e);
     }
 
     public chartHovered(e: any): void {
         console.log(e);
+    }
+
+    private calculateStorageData(): void {
+        this.storageService.getRecords().then(storage => {
+            if(!storage || Object.keys(storage).length === 0){
+                return null; //todo: display error and navigate back;
+            }
+
+            this.doughnutChartLabels = Object.keys(storage);
+
+            this.doughnutChartData = this.doughnutChartLabels.map(label => {
+                const records = storage[label];
+                return records.reduce((acc, record) => {
+                    return acc + record.performedTime;
+                }, 0);
+            })
+        });
     }
 }
