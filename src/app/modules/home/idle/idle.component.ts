27b8cf64cc3afd038870bb3777bdace57128a2c6
sumragen <Sumragen@gmail.com>
2019-08-14 21:37:00 +0300

    added custom type buttons handler

--- src/app/modules/home/idle/idle.component.ts
+++ src/app/modules/home/idle/idle.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
 import { RecordInterface } from '../../../shared/models/record';
 import { StorageService } from '../../../shared/services/storage/storage.service';
 
@@ -8,9 +9,9 @@ import { StorageService } from '../../../shared/services/storage/storage.service
    styleUrls: ['./idle.component.scss'],
 })
 export class IdleComponent implements OnInit {
-   @Input() public record: RecordInterface;
    public types: string[] = [];
-
+   @Input() public record: RecordInterface;
+   @Output() public predefinedType: EventEmitter<string> = new EventEmitter();
    constructor(private storageService: StorageService) { }
 
    ngOnInit() {
@@ -20,6 +21,11 @@ export class IdleComponent implements OnInit {
          }
          this.types = types.splice(0, 4); // todo: that should be complexity calculation of popular types
       });
+   }
+
+   public predefinedTypeClick(type: string): void {
+      this.record.type = type;
+      this.predefinedType.emit(type);
    }
 
 }
