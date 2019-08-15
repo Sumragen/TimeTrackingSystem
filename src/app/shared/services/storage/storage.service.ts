0b90703706b9fdbb022a63b75c8143ee17cfeeb1
sumragen <Sumragen@gmail.com>
2019-08-15 08:54:02 +0300

    fixed incorrect handling of case when there is no items in storage for device

--- src/app/shared/services/storage/storage.service.ts
+++ src/app/shared/services/storage/storage.service.ts
@@ -1,52 +1,57 @@
-import {Injectable} from "@angular/core";
-import {NativeStorage} from "@ionic-native/native-storage/ngx";
-import {Platform} from "@ionic/angular";
+import { Injectable } from '@angular/core';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
+import { Platform } from '@ionic/angular';
 
 export const recordStorageKey: string = 'record_storage';
 
 @Injectable({
-    providedIn: 'root'
+   providedIn: 'root'
 })
 export class StorageService {
-    constructor(private storage: NativeStorage,
-                private platform: Platform) {
-    }
+   constructor(private storage: NativeStorage,
+               private platform: Platform) {
+   }
 
-    public isBrowser(): boolean {
-        return this.platform.is('mobileweb');
-    }
+   public isBrowser(): boolean {
+      return this.platform.is('mobileweb');
+   }
 
-    public async getKeys(): Promise<string[]> {
-        const storage = await this.getRecords();
+   public async getKeys(): Promise<string[]> {
+      const storage = await this.getRecords();
 
-        if (!storage || Object.keys(storage).length === 0) {
-            return;
-        }
-        return Object.keys(storage);
-    }
+      if (!storage || Object.keys(storage).length === 0) {
+         return;
+      }
+      return Object.keys(storage);
+   }
 
-    public getRecords(): Promise<any> {
-        return this.getItem(recordStorageKey);
-    }
+   public getRecords(): Promise<any> {
+      return this.getItem(recordStorageKey);
+   }
 
-    public setRecords(storage: any): Promise<any> {
-        return this.setItem(recordStorageKey, storage);
-    }
+   public setRecords(storage: any): Promise<any> {
+      return this.setItem(recordStorageKey, storage);
+   }
 
-    public getItem<T>(key: string): Promise<T> {
-        if (this.isBrowser()) {
-            return Promise.resolve().then(() => JSON.parse(localStorage.getItem(key)));
-        } else {
-            return this.storage.getItem(key);
-        }
-    }
+   public getItem<T>(key: string): Promise<T> {
+      if (this.isBrowser()) {
+         return Promise.resolve().then(() => JSON.parse(localStorage.getItem(key)));
+      } else {
+         return this.storage.getItem(key)
+            .catch(reason => {
+               if (reason.code === 2) {
+                  return null;
+               }
+            });
+      }
+   }
 
-    public setItem<T>(key: string, value: T): Promise<void> {
-        if (this.isBrowser()) {
-            return Promise.resolve().then(() => localStorage.setItem(key, JSON.stringify(value)));
-        } else {
-            return this.storage.setItem(key, value);
-        }
+   public setItem<T>(key: string, value: T): Promise<void> {
+      if (this.isBrowser()) {
+         return Promise.resolve().then(() => localStorage.setItem(key, JSON.stringify(value)));
+      } else {
+         return this.storage.setItem(key, value);
+      }
 
-    }
+   }
 }
