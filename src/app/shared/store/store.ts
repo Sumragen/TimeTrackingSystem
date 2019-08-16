cf180bab9ab5193f04ca7b6a174369d5b573aa3f
sumragen <Sumragen@gmail.com>
2019-08-16 18:40:50 +0300

    feature: temporary adapt store

--- src/app/shared/store/store.ts
+++ src/app/shared/store/store.ts
@@ -5,7 +5,7 @@ import { activityReducer, ActivityState } from './reducers/activity.reducer';
 export const ACTIVITY_STATE_KEY: string = 'activity'; // TODO find way how to remove it
 
 export interface STORE_STATE {
-   activity: ActivityState,
+   activity?: ActivityState,
 }
 
 export interface StoreAction extends PayloadAction<STORE_STATE> {}
@@ -15,5 +15,4 @@ export interface PayloadAction<T> extends ActionCreator {
 }
 
 export const reducers: ActionReducerMap<STORE_STATE, Action> = {
-   activity: activityReducer
 };
