2868d326f5e4c5e56e96ed380ee2679b233852d4
sumragen <Sumragen@gmail.com>
2019-08-16 18:39:56 +0300

    feature: added effects for the activity feature

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 import { HomePage } from './home.page';
 import { IdleComponent } from './idle/idle.component';
 import { PerformComponent } from './perform/perform.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { activityReducer } from '../../shared/store/reducers/activity.reducer';
+import { ACTIVITY_STATE_KEY } from '../../shared/store/store';
+import { StorageEffect } from '../../shared/store/effects/storage.effect';
+import { ActivityEffect } from '../../shared/store/effects/activity.effect';
 
 @NgModule({
    imports: [
@@ -15,6 +20,8 @@ import { HomeRoutingModule } from './home-routing.module';
       FormsModule,
       IonicModule,
       HomeRoutingModule,
+      StoreModule.forFeature(ACTIVITY_STATE_KEY, activityReducer),
+      EffectsModule.forFeature([StorageEffect, ActivityEffect])
    ],
    declarations: [
       HomePage,
---
+++ src/app/shared/store/effects/activity.effect.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { Router } from '@angular/router';
+import { ActivityActionsKey } from '../reducers/activity.reducer';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class ActivityEffect {
+   constructor(private actions$: Actions,
+               private router: Router) {
+   }
+
+   perform$ =
+      createEffect(() => this.actions$.pipe(
+         ofType(ActivityActionsKey.PERFORM),
+         tap(() => this.router.navigate(['/home/perform']))
+      ));
+
+   complete$ =
+      createEffect(() => this.actions$.pipe(
+         ofType(ActivityActionsKey.COMPLETE),
+         tap(() => this.router.navigate(['/home/idle']))
+      ));
+}
