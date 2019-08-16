d33fd778de2b285feaa49a4016b9a611d9e9a121
sumragen <Sumragen@gmail.com>
2019-08-16 21:46:14 +0300

    feature: moved hardcoded array into specified variable

--- src/app/shared/store/meta/storage.meta-reducer.ts
+++ src/app/shared/store/meta/storage.meta-reducer.ts
@@ -6,9 +6,10 @@ import { StorageService } from '../../services/storage/storage.service';
 export function storageMetaReducer<S, A extends Action = Action>(storageService: StorageService) {
    return function (reducer: ActionReducer<S, A>) {
       return function (state: S, action: A): S {
+         const systemActions: string[] = [INIT, ROOT_EFFECTS_INIT, UPDATE];
          const nextState = reducer(state, action);
 
-         if ([INIT, ROOT_EFFECTS_INIT, UPDATE].indexOf(action.type) === -1) {
+         if (systemActions.indexOf(action.type) === -1) {
             storageService.setSavedState(nextState);
          }
 
