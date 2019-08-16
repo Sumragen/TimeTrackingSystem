0accfb2f9bb45989009311f15f1aa5cda40c827d
sumragen <Sumragen@gmail.com>
2019-08-16 19:04:17 +0300

    feature: cleanup home page

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+
 import { HomePage } from './home.page';
 import { IdleComponent } from './idle/idle.component';
 import { PerformComponent } from './perform/perform.component';
--- src/app/modules/home/home.page.ts
+++ src/app/modules/home/home.page.ts
@@ -1,10 +1,4 @@
 import { Component } from '@angular/core';
-import { select, Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-
-import { ACTIVITY_STATUS, ActivityState } from '../../shared/store/reducers/activity.reducer';
-import { ACTIVITY_STATE_KEY, STORE_STATE } from '../../shared/store/store';
-import { STORAGE_EFFECT } from '../../shared/store/effects/storage.effect';
 
 @Component({
    selector: 'app-home',
@@ -12,25 +6,5 @@ import { STORAGE_EFFECT } from '../../shared/store/effects/storage.effect';
    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-   constructor(
-      private store: Store<STORE_STATE>
-   ) {}
-
-   public applyActivityType(target: ACTIVITY_STATUS, type?: string) {
-      this.store.dispatch({
-         type: STORAGE_EFFECT.RECORD,
-         payload: {
-            target,
-            type
-         }
-      });
-   }
-
-   public isInIdle(status: ACTIVITY_STATUS): boolean {
-      return status === ACTIVITY_STATUS.IDLE;
-   }
-
-   public isInPerform(status: ACTIVITY_STATUS): boolean {
-      return status === ACTIVITY_STATUS.PERFORM;
-   }
+   constructor() {}
 }
