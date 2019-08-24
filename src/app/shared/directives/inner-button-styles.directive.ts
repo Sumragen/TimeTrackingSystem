84cbd5ad261c2d22e93213ad0191867d24b1f64e
sumragen <Sumragen@gmail.com>
2019-08-24 22:47:15 +0300

    feature: type input component - styling

--- src/app/shared/directives/inner-button-styles.directive.ts
+++ src/app/shared/directives/inner-button-styles.directive.ts
@@ -1,16 +1,31 @@
 import { Directive, HostBinding, Input } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
+
+import { HLColor } from '../models/colors.models';
 
 @Directive({
   selector: '[appInnerButtonStyles]'
 })
 export class InnerButtonStylesDirective {
-  @Input('appInnerButtonStyles') color: string;
+  @Input('appInnerButtonStyles') color: HLColor;
 
   constructor(private sanitizer: DomSanitizer) {}
 
   @HostBinding('attr.style')
-  public get valueAsStyle(): any {
-    return this.sanitizer.bypassSecurityTrustStyle(`--background:${this.color}`);
+  public get valueAsStyle(): SafeStyle {
+    const color: HLColor = this.color;
+    const border: string = `hsla(${color.hue}, 100%, ${color.luminance}%, 1)`;
+    const bg: string = `hsla(${color.hue}, 100%, ${color.luminance}%, 0.3)`;
+    const bgActivated: string = `hsla(${color.hue}, 100%, ${color.luminance - 10}%, 1)`;
+
+    const style = `
+      --border-color:${border};
+      --background:${bg};
+      --background-activated:${bgActivated};
+      --color:black;
+      --color-activated:black;
+    `;
+
+    return this.sanitizer.bypassSecurityTrustStyle(style);
   }
 }
