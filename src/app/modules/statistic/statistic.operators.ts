a411b18f1c54cb55d38e710058f8a71bb391c7e2
sumragen <Sumragen@gmail.com>
2019-08-27 18:58:23 +0300

    refactor: statistic resolver - decomposition of methods

--- src/app/modules/statistic/statistic.operators.ts
+++ src/app/modules/statistic/statistic.operators.ts
@@ -4,32 +4,41 @@ import { HLColor } from '../../shared/models/colors.models';
 
 const mapArrayByProp = (property: string) => map(prop(property));
 
+const convertPairedStorageToObject = map(([label, { color, data }]: any) => ({
+  label,
+  color,
+  data
+}));
+
+const getLabels = mapArrayByProp('label');
+const getColors = mapArrayByProp('color');
+const getData = mapArrayByProp('data');
+
+const convertChartPropsToObject = (
+  labels: string[],
+  colors: HLColor[],
+  data: number[]
+): ChartData => ({
+  labels,
+  colors,
+  data
+});
+
+const getPerformedTimeData = pipe(
+  mapArrayByProp('performedTime'),
+  // @ts-ignore
+  reduce(add, 0)
+);
+
 export const convertActivityStorageToChartData = pipe(
   toPairs,
-  map(([label, { color, data }]: any) => ({
-    label,
-    color,
-    data
-  })),
-  converge(
-    (labels: string[], colors: HLColor[], data: number[]): ChartData => ({
-      labels,
-      colors,
-      data
-    }),
-    [
-      mapArrayByProp('label'),
-      mapArrayByProp('color'),
-      pipe(
-        mapArrayByProp('data'),
-        map(
-          pipe(
-            mapArrayByProp('performedTime'),
-            // @ts-ignore
-            reduce(add, 0)
-          )
-        )
-      )
-    ]
-  )
+  convertPairedStorageToObject,
+  converge(convertChartPropsToObject, [
+    getLabels,
+    getColors,
+    pipe(
+      getData,
+      map(getPerformedTimeData)
+    )
+  ])
 );
