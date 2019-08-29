1fe2787a2c28c676dcdfdd775f3e0074c45e7c50
sumragen <Sumragen@gmail.com>
2019-08-29 21:05:10 +0300

    refactor: activity dispatch - used utils methods for payload

--- src/app/modules/activity/store/activity.dispatch.ts
+++ src/app/modules/activity/store/activity.dispatch.ts
@@ -7,63 +7,53 @@ import {
   CompleteActivityAction,
   SwitchActivityAction
 } from '../models/activity-action.types';
+import { PayloadAction, PayloadTargetAction } from '../../../shared/store/store';
+import { Activity } from '../models/activity.types';
 
 @Injectable()
 export class ActivityDispatch {
   @Dispatch()
   public updateType(type: string) {
-    return {
-      type: ActivityActionsKey.SET_TYPE,
-      payload: {
-        type
-      }
-    };
+    return ActivityDispatch.payload(ActivityActionsKey.SET_TYPE, { type });
   }
 
   @Dispatch()
   public completeActivity(): CompleteActivityAction {
-    return {
-      type: STORAGE_EFFECT.COMPLETE,
-      payload: {
-        target: {
-          type: ActivityActionsKey.COMPLETE
-        }
-      }
-    };
+    return ActivityDispatch.payloadTarget(STORAGE_EFFECT.COMPLETE, {
+      type: ActivityActionsKey.COMPLETE
+    });
   }
 
   @Dispatch()
   public switchActivity(activityType: string): SwitchActivityAction {
-    return {
-      type: STORAGE_EFFECT.COMPLETE,
-      payload: {
-        target: {
-          type: STORAGE_EFFECT.LOG_TIME,
-          payload: {
-            target: {
-              type: ActivityActionsKey.PERFORM,
-              payload: {
-                type: activityType
-              }
-            }
-          }
-        }
-      }
-    };
+    return ActivityDispatch.payloadTarget(
+      STORAGE_EFFECT.COMPLETE,
+      ActivityDispatch.payloadTarget(
+        STORAGE_EFFECT.LOG_TIME,
+        ActivityDispatch.payload(ActivityActionsKey.PERFORM, {
+          type: activityType
+        })
+      )
+    );
   }
 
   @Dispatch()
   public applyActivityType(type: string): ApplyActivityAction {
+    return ActivityDispatch.payloadTarget(
+      STORAGE_EFFECT.LOG_TIME,
+      ActivityDispatch.payload<Activity>(ActivityActionsKey.PERFORM, { type })
+    );
+  }
+
+  private static payload<T>(type: string, payload: T): PayloadAction<T> {
     return {
-      type: STORAGE_EFFECT.LOG_TIME,
-      payload: {
-        target: {
-          type: ActivityActionsKey.PERFORM,
-          payload: {
-            type
-          }
-        }
-      }
+      type,
+      payload
     };
+  }
+  private static payloadTarget<T>(type: string, target: T): PayloadTargetAction<T> {
+    return ActivityDispatch.payload(type, {
+      target
+    });
   }
 }
