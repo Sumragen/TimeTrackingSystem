12777e87a0d422eca23f62786e395f13a82c6039
sumragen <Sumragen@gmail.com>
2019-08-29 23:28:55 +0300

    refactor: activity module - removed excess component TypeInput

--- src/app/modules/activity/activity.module.ts
+++ src/app/modules/activity/activity.module.ts
@@ -9,9 +9,9 @@ import { ACTIVITY_STATE_KEY } from '../../shared/store/store';
 import { StorageEffect } from '../../shared/store/effects/storage.effect';
 import { SecondsToClockFormatPipe } from '../../shared/pipes/time/seconds-to-clock-format.pipe';
 import { SharedModule } from '../../shared/shared.module';
+
 import { PerformComponent } from './components/perform/perform.component';
 import { StopwatchComponent } from './components/perform/stopwatch/stopwatch.component';
-import { TypeInputComponent } from './components/type-input/type-input.component';
 import { TypeSelectorComponent } from './components/type-selector/type-selector.component';
 import { activityReducer } from './store/activity.reducer';
 import { ActivityEffect } from './store/activity.effect';
@@ -34,7 +34,6 @@ import { ActivityDispatch } from './store/activity.dispatch';
   declarations: [
     ActivityPage,
     PerformComponent,
-    TypeInputComponent,
     StopwatchComponent,
     SecondsToClockFormatPipe,
     TypeSelectorComponent,
