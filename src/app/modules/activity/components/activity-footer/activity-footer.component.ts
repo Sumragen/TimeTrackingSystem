37a679ca21389d5df6b16ae70ce09fd56a670b65
sumragen <Sumragen@gmail.com>
2019-08-29 23:10:32 +0300

    feature: activity page - made method getActionText more easy to read

--- src/app/modules/activity/components/activity-footer/activity-footer.component.ts
+++ src/app/modules/activity/components/activity-footer/activity-footer.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { IonInput } from '@ionic/angular';
 import { Actions, ofType } from '@ngrx/effects';
-import { always, cond, equals, ifElse, T } from 'ramda';
+import { always, cond, equals, T } from 'ramda';
 import { Observable } from 'rxjs';
 import { map, startWith, switchMap } from 'rxjs/operators';
 
@@ -69,7 +69,8 @@ export class ActivityFooterComponent implements OnInit {
 
   public getActionText = cond([
     [this.isIdle, always('start')],
-    [T, ifElse(this.shouldSwitchActivity, always('switch'), always('stop'))]
+    [this.shouldSwitchActivity, always('switch')],
+    [T, always('stop')]
   ]);
 
   private shouldSwitchActivity(): boolean {
